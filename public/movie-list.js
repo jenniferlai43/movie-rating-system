71fae1af7c5b7f7a802edbd9aa7e96c9f7d86869
jenniferlai43 <jenniferlai43@gmail.com>
2018-07-31 15:26:33 -0700

    Modified some of frontend. - Made "Add Movie" button trigger collapsible form - Added transparent hover to movies - Added nonfunctional delete button (will be implemented)

--- public/movie-list.js
+++ public/movie-list.js
@@ -1,6 +1,12 @@
-// AJAX
-$(document).ready(function{
-	$('#movie').on('click', function() {
-		
-	});
-});
+function collapse()
+{
+	var div = document.getElementsByClassName("collapse_form");
+	if (div[0].style.display == "none")
+	{
+		div[0].style.display = "block";
+	}
+	else
+	{
+		div[0].style.display = "none";
+	}
+}
